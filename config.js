72536733eaaa7c18f8800d2ef24edeed779fdd72
phamducson14 <63386521+phamducson14@users.noreply.github.com>
2024-11-20 23:30:38 +0700

    Add files via upload
    creat

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Hương Giang';
+const giftUrl = 'https://www.facebook.com/huonggiang2111';
+const eventName = 'Happy Birthay';
+const titleCard = 'Happy Birthay';
+const contentCard = 'Chúc mừng ngày đặc biệt của em. Chúc em tuổi mới luôn mạnh khỏe, xinh đẹp, hạnh phúc, vạn sự như ý, phát tài phát lộc. Chúc em một ngày sinh nhật vui vẻ và trọn vẹn niềm vui. Happy birthday !!!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
